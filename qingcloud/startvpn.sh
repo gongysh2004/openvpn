2ac75e3d228dbb43138a3304cc6f5e170c0b053c
gong yong sheng <gong.yongsheng@99cloud.net>
2015-05-03 18:30:32 +0800

    make qingcloud script more robus

--- qingcloud/startvpn.sh
+++ qingcloud/startvpn.sh
@@ -18,14 +18,49 @@ echo "Start instance $vpninsid ..."
 ret=`qingcloud iaas start-instances -i $vpninsid`
 check_retcode "$ret"
 
+echo "Wait for instance $vpninsid to start ..."
+ret=`qingcloud iaas describe-instances -i $vpninsid`
+check_retcode "$ret"
+status=`echo -e "$ret" | grep -e '\"status\":' | awk -F\" '{ print $4}'`
+while :
+do
+   echo "$status"
+   if [[ "$status" = "running" ]]; then
+     echo "running checked!"
+     break
+   else
+     echo -e ".\c"
+     ret=`qingcloud iaas describe-instances -i $vpninsid`
+     check_retcode "$ret"
+     status=`echo -e "$ret" | grep -e '\"status\":' | awk -F\" '{ print $4}'`
+   fi
+done
 echo 'Allocate eip ...'
 ret=`qingcloud iaas allocate-eips -n test -b 4 -B bandwidth`
 check_retcode "$ret"
 eip=`echo -e $ret |  grep -e "eip-[^\"]*" -o`
 
-echo 'Associate eip $eip ...'
+echo "Associate eip $eip ..."
 ret=`qingcloud iaas associate-eip -e $eip -i $vpninsid`
 check_retcode "$ret"
 
-echo 'Get EIP address $eip ...'
+echo "Checking eip $eip is associated..."
+ret=`qingcloud iaas describe-eips -e $eip`
+check_retcode "$ret"
+status=`echo -e "$ret" | grep -e '\"status\":' | awk -F\" '{ print $4}'`
+while :
+do
+   echo "$status"
+   if [[ "$status" = "associated" ]]; then
+     echo "associated checked!"
+     break
+   else
+     echo -e ".\c"
+     ret=`qingcloud iaas describe-eips -e $eip`
+     check_retcode "$ret"
+     status=`echo -e "$ret" | grep -e '\"status\":' | awk -F\" '{ print $4}'`
+   fi
+done
+
+echo "Get EIP address $eip ..."
 qingcloud iaas describe-instances -i $vpninsid | grep eip_addr | awk -F\" '{ print $4}'
--- qingcloud/stopvpn.sh
+++ qingcloud/stopvpn.sh
@@ -1,7 +1,7 @@
 vpninsid='i-rj8b03a6'
 function echo_and_exit()
 {
-    echo $1
+    echo -e "$1"
     if [[ "$1" != "0" ]]; then
         echo -e "\tFailed"
         exit 1
@@ -18,16 +18,36 @@ echo "Get eip of instance $vpninsid ..."
 ret=`qingcloud iaas describe-instances -i $vpninsid`
 check_retcode "$ret"
 eip=`echo $ret | grep  -o -e 'eip-[a-z0-9]\+'`
-echo "eip $eip"
-
-echo 'Disociate eip $eip ...'
-ret=`qingcloud iaas dissociate-eips -e $eip`
-check_retcode "$ret"
-
-echo 'Stop instance $vpninsid ...'
+echo "eip:${eip}-"
+if [[ "y${eip}y" != "yy" ]]; then
+    echo "Disociate eip $eip ..."
+    ret=`qingcloud iaas dissociate-eips -e $eip`
+    check_retcode "$ret"
+    
+    echo "Checking eip $eip is disociated..."
+    ret=`qingcloud iaas describe-eips -e $eip`
+    check_retcode "$ret"
+    status=`echo -e "$ret" | grep -e '\"status' | awk -F\" '{ print $4}'`
+    while :
+    do
+       echo "$status"
+       if [[ "$status" = "available" ]]; then
+         break
+         echo "disociated checked!"
+       else
+         echo -e ".\c"
+         ret=`qingcloud iaas describe-eips -e $eip`
+         check_retcode "$ret"
+         status=`echo -e "$ret" | grep -e '\"status' | awk -F\" '{ print $4}'`
+       fi
+    done
+fi
+echo "Stop instance $vpninsid ..."
 ret=`qingcloud iaas stop-instances -i $vpninsid`
 check_retcode "$ret"
 
-echo 'Release Eip $eip ...'
-ret=`qingcloud iaas release-eips -F 1  -e $eip`
-check_retcode "$ret"
+if [[ "y${eip}y" != "yy" ]]; then
+    echo "Release Eip $eip ..."
+    ret=`qingcloud iaas release-eips -F 1  -e $eip`
+    check_retcode "$ret"
+fi
